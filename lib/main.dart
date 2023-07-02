d40251007f86ac933acc111b23b5c5547a0a4e43
Giovanni Varde <giova9975@gmail.com>
2023-07-02 18:49:36 +0000

    nuova bottom tabbar bella, però ancora non c'è lo swipe. In cartella common lascio i modelli delle altre tabbar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,15 @@
+import 'package:gitpod_flutter_quickstart/src/models/bottom_bar_item_model.dart';
+import 'package:gitpod_flutter_quickstart/src/notch_bottom_bar_controller.dart';
+import 'package:gitpod_flutter_quickstart/common/notch_bottom_bar.dart';
+
+import 'common/notch_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:awesome_bottom_bar/awesome_bottom_bar.dart';
-import 'common/bottom_bar_jolomon.dart';
-import 'common/bottom_tabbar_jolomon.dart';
 
-const List<TabItem> items = [
-  TabItem(
-    icon: FontAwesomeIcons.barsStaggered,
-    title: 'Liste',
-  ),
-  TabItem(
-    icon: FontAwesomeIcons.message,
-    title: 'Notifiche',
-  ),
-];
-
-void main() => runApp(ComicaniApp());
+void main() => runApp(ComicaniApp()); //run the app
 
 class ComicaniApp extends StatelessWidget {
+  //create the app as extension of the statelessWidget; closed at line 39
   const ComicaniApp({Key? key}) : super(key: key);
 
   @override
@@ -35,25 +27,46 @@ class ComicaniApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  //creates the home page as extension of statefulWidget; ends at line 46
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<
+        MyHomePage> //MAIN ATTRACTION: here we define the widgets; ends at the end of main.dart
+    with
+        TickerProviderStateMixin {
   int _selectedIndex = 0;
 
-  static final List<Widget> _widgetOptions = <Widget>[
-    const Text('Liste'),
-    const Text('Notifiche'),
+  /*static final List<Widget> _widgetOptions = <Widget>[
+    const Text('xd'),
+    const Text('xp'),
+  ];*/
+  static const _kTabPages = <Widget>[
+    Center(child: Icon(Icons.cloud, size: 64.0, color: Colors.teal)),
+    Center(child: Icon(Icons.alarm, size: 64.0, color: Colors.cyan)),
   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _notchBottomBarController = NotchBottomBarController(
+      index: _selectedIndex,
+    );
+    _tabController = TabController(length: 2, vsync: this);
   }
+
+  @override
+  void dispose() {
+    _notchBottomBarController.dispose();
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  late NotchBottomBarController _notchBottomBarController;
+  late TabController _tabController;
 
   @override
   Color colorSelect = const Color(0XFF0686F8);
@@ -61,23 +74,45 @@ class _MyHomePageState extends State<MyHomePage> {
   Color color2 = const Color(0XFF96B1FD);
   Color bgColor = const Color(0XFF1752FE);
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+      body: TabBarView(
+        controller: _tabController,
+        children: _kTabPages,
       ),
-      bottomNavigationBar: BottomTabbarJolomon(
-        //for sliding pages, substitute bottomnavbar with tabbar: do as in https://x-wei.github.io/flutter_catalog/#/minified:L_
-        items: items,
-        color: Colors.blue,
-        backgroundColor: Colors.white,
-        colorSelected: Colors.white,
-        backgroundSelected: Colors.blue,
-        borderRadius: BorderRadius.circular(0),
-        indexSelected: _selectedIndex,
-        onTap: (index) => setState(() {
-          _selectedIndex = index;
-        }),
+      bottomNavigationBar: AnimatedNotchBottomBar(
+        notchBottomBarController: _notchBottomBarController,
+        bottomBarItems: const [
+          BottomBarItem(
+            inActiveItem: Icon(
+              Icons.home_filled,
+              color: Colors.blueGrey,
+            ),
+            activeItem: Icon(
+              Icons.home_filled,
+              color: Colors.blueAccent,
+            ),
+            itemLabel: 'Page 1',
+          ),
+          BottomBarItem(
+            inActiveItem: Icon(
+              Icons.star,
+              color: Colors.blueGrey,
+            ),
+            activeItem: Icon(
+              Icons.star,
+              color: Colors.blueAccent,
+            ),
+            itemLabel: 'Page 2',
+          ),
+        ],
+        onTap: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        showLabel: false,
       ),
     );
   }
